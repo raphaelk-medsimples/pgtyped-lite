f9b9f752a9d87b5b86e06721896968705b72fca7
Raphael Kikuchi <raphael.kikuchi@medsimples.com.br>
2024-12-15 03:04:12 -0300

    v2.4.3

--- packages/cli/src/worker.ts
+++ packages/cli/src/worker.ts
@@ -2,13 +2,37 @@ import fs from 'fs-extra';
 import nun from 'nunjucks';
 import path from 'path';
 import worker from 'piscina';
+
+import { PGlite } from '@electric-sql/pglite';
+import { adminpack } from '@electric-sql/pglite/contrib/adminpack';
+import { amcheck } from '@electric-sql/pglite/contrib/amcheck';
+import { auto_explain } from '@electric-sql/pglite/contrib/auto_explain';
+import { bloom } from '@electric-sql/pglite/contrib/bloom';
+import { btree_gin } from '@electric-sql/pglite/contrib/btree_gin';
+import { btree_gist } from '@electric-sql/pglite/contrib/btree_gist';
+import { citext } from '@electric-sql/pglite/contrib/citext';
+import { cube } from '@electric-sql/pglite/contrib/cube';
+import { earthdistance } from '@electric-sql/pglite/contrib/earthdistance';
+import { fuzzystrmatch } from '@electric-sql/pglite/contrib/fuzzystrmatch';
+import { hstore } from '@electric-sql/pglite/contrib/hstore';
+import { isn } from '@electric-sql/pglite/contrib/isn';
+import { lo } from '@electric-sql/pglite/contrib/lo';
+import { ltree } from '@electric-sql/pglite/contrib/ltree';
+import { pg_trgm } from '@electric-sql/pglite/contrib/pg_trgm';
+import { seg } from '@electric-sql/pglite/contrib/seg';
+import { tablefunc } from '@electric-sql/pglite/contrib/tablefunc';
+import { tcn } from '@electric-sql/pglite/contrib/tcn';
+import { uuid_ossp } from '@electric-sql/pglite/contrib/uuid_ossp';
+import { tsm_system_time } from '@electric-sql/pglite/contrib/tsm_system_time';
+import { tsm_system_rows } from '@electric-sql/pglite/contrib/tsm_system_rows';
+
 import { ParsedConfig, TransformConfig } from './config.js';
 import {
   generateDeclarationFile,
   generateTypedecsFromFile,
 } from './generator.js';
 import { TypeAllocator, TypeMapping, TypeScope } from './types.js';
-import { PGlite } from '@electric-sql/pglite';
+
 
 // disable autoescape as it breaks windows paths
 // see https://github.com/adelsz/pgtyped/issues/519 for details
@@ -50,7 +74,31 @@ export async function getTypeDecs({
 }) {
   const contents = await connectAndGetFileContents(fileName);
   const types = new TypeAllocator(TypeMapping(config.typesOverrides));
-  const connection = new PGlite('memory://');
+  const connection = new PGlite('memory://', {
+    extensions: {
+      adminpack,
+      amcheck,
+      auto_explain,
+      bloom,
+      btree_gin,
+      btree_gist,
+      citext,
+      cube,
+      earthdistance,
+      fuzzystrmatch,
+      hstore,
+      isn,
+      lo,
+      ltree,
+      pg_trgm,
+      seg,
+      tablefunc,
+      tcn,
+      uuid_ossp,
+      tsm_system_time,
+      tsm_system_rows,
+    }
+  });
   await connection.query('select 1');
   await connection.exec(fs.readFileSync(config.migrationsFile).toString());
   
